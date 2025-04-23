5723517f8feb936dfa9a0d2fda299001d2f28664
Anderson Campos Padilha <campospadilha.a@gmail.com>
2025-04-23 09:18:59 -0300

    Criado teste de remoção de registro

--- src/test/java/br/campospadilhaa/tasks/apitest/APITest.java
+++ src/test/java/br/campospadilhaa/tasks/apitest/APITest.java
@@ -1,5 +1,7 @@
 package br.campospadilhaa.tasks.apitest;
 
+import java.net.MalformedURLException;
+
 import org.hamcrest.CoreMatchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -82,5 +84,38 @@ public class APITest {
 				.log().all()
 				.statusCode(400)
 				.body("message", CoreMatchers.is("Due date must not be in past"));
+	}
+
+	@Test
+	public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+
+		// given: dado
+		// when: quando
+		// then: então
+
+		// inserindo tarefa
+		Integer id = 
+			RestAssured
+				.given()
+					.body("{\"task\": \"Nome da tarefa para remoção\", \"dueDate\": \"2030-12-30\"}")
+					.contentType(ContentType.JSON)
+				.when()
+					.post("/todo")
+				.then()
+					.log().all()
+					.statusCode(201)
+	
+					// obtenção do atributo 'id' para remover
+					.extract().path("id");
+
+		System.out.println(id);
+
+		// remover a tarefa inserida
+		RestAssured
+			.given()
+			.when()
+				.delete("/todo/"+id)
+			.then()
+				.statusCode(204);
 	}
 }
